7c95a6ed15cf0e5d46ec7aa7b85c215126a644a6
Drew Wyatt <drew.j.wyatt@gmail.com>
2020-12-29 13:12:38 -0500

    add more tests

--- src/main.test.ts
+++ src/main.test.ts
@@ -4,4 +4,12 @@ describe('main', () => {
   it('does not return anything', () => {
     expect(main()).toBeUndefined()
   })
+
+  it('has another test', () => {
+    expect(true).toEqual(true)
+  })
+
+  it('has a third test that should always pass', () => {
+    expect(null).toBeFalsy()
+  })
 })
