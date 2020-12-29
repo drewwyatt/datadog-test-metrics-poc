8e79d5045c0593fbb0ef230ab4a69d6cca9078fb
Drew Wyatt <drew.j.wyatt@gmail.com>
2020-12-29 12:25:15 -0500

    add custom reporter

--- src/reporter.ts
+++ src/reporter.ts
@@ -1,8 +1,42 @@
-import { Reporter, Context } from '@jest/reporters'
+import * as core from '@actions/core'
+import { Reporter as JestReporter, Context } from '@jest/reporters'
 import { AggregatedResult } from '@jest/test-result'
+import isCI from 'is-ci'
 
-export default class CustomReporter implements Pick<Reporter, 'onRunComplete'> {
+type Reporter = Pick<JestReporter, 'onRunComplete'>
+
+const OUTPUT_PROPS: ReadonlyArray<keyof AggregatedResult> = [
+  'numFailedTests',
+  'numFailedTestSuites',
+  'numPassedTests',
+  'numPassedTestSuites',
+  'numPendingTests',
+  'numTodoTests',
+  'numPendingTestSuites',
+  'numRuntimeErrorTestSuites',
+  'numTotalTests',
+  'numTotalTestSuites',
+  'snapshot',
+  'startTime',
+  'success',
+  'wasInterrupted',
+]
+
+class ActionsReporter implements Reporter {
   async onRunComplete(_: Set<Context>, results: AggregatedResult) {
-    console.log('Your report is available!')
+    core.info('Sending aggregate results to ouput...')
+    for (let key of OUTPUT_PROPS) {
+      core.info(`Setting ${key}...`)
+      core.setOutput(key, results[key])
+    }
+    core.info('Done!')
   }
 }
+
+class FallbackReporter implements Reporter {
+  async onRunComplete() {
+    console.log('CI run not detected, skipping...')
+  }
+}
+
+export default isCI ? ActionsReporter : FallbackReporter
